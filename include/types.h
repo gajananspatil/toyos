a08cc73da7146123d49735b07a99866a56f928a3
Gajanan Patil <gajanansp@gmail.com>
2017-01-30 17:48:05 +0530

    Create types.h

---
+++ include/types.h
@@ -0,0 +1,15 @@
+#ifndef _TYPES_
+#define _TYPES_
+
+
+typedef char 			sint_8;
+typedef unsigned char 	uint_8;
+typedef signed short	sint_16;
+typedef unsigned short	uint_16;
+typedef	int				sint_32;
+typedef unsigned int	uint_32;
+typedef long			sint_64;
+typedef unsigned long	uint_64;
+typedef uint_32			size_t;
+
+#endif
